f947d48040e88bfd3822b737ae006083f83197f2
geeks2champ <geeks2champ@gmail.com>
2015-12-26 22:42:33 +0530

    Reverse a linked list without recursion

---
+++ ReverseLinkList (Iterative)/src/main/java/org/learn/List/Node.java
@@ -0,0 +1,10 @@
+package org.learn.List;
+
+public class Node {	
+	public int data;
+	public Node next;
+	public Node(int num) {
+		this.data = num;
+		this.next = null;
+	}
+}
---
+++ ReverseLinkList (Iterative)/src/main/java/org/learn/Question/ReverseList.java
@@ -0,0 +1,30 @@
+package org.learn.Question;
+
+import org.learn.List.Node;
+
+public class ReverseList {
+	public static Node reverseList(Node head) {
+		Node next = null, prev = null; 
+		while(head != null) {
+			next = head.next;
+			head.next = prev;
+			prev = head;
+			head = next;
+		}
+		return prev;
+	}
+	
+	public static void insert(Node head, int data) {
+		while(head.next != null) 
+			head = head.next;
+		head.next = new Node(data);
+	}
+	
+	public static void print(Node head) {
+		while(head != null) {
+			System.out.printf("%d ",head.data);
+			head = head.next;
+		}
+		System.out.println("");		
+	}
+}
